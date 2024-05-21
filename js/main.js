2f00712dbe20d5f3f122f2cb77273815191e1627
workmoon <108781493+workmoon@users.noreply.github.com>
2024-05-21 22:22:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.tab-li').on('click', function(){
+    $('.tab-li').removeClass('active');
+    $(this).addClass('active');
+})
+
+$('.tab-list').on('click', function(){
+    $('.tab-list').removeClass('active');
+    $(this).addClass('active');
+})
+
+$('.cal-tab ul li').on('click', function(){
+    $('.cal-tab ul li').removeClass('active');
+    $(this).addClass('active');
+})
+
+$('.days-squear').on('click', function(){
+    $('.days-squear').removeClass('active');
+    $(this).addClass('active');
+})
